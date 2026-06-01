d13bfc093f61d70ee2c89617e3583b083b60c249
SandroNogueiraBittencourt <sandronogueirabittencourt@outlook.com>
2026-06-01 20:09:06 -0300

    Atualiza identidade de marca Login

--- gerenciador-senhas-frontend/src/pages/Login.jsx
+++ gerenciador-senhas-frontend/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { LockKeyhole } from "lucide-react";
 import { loginUsuario } from "../services/authService";
+import logoNexoVault from "../assets/logo-nexovault.png";
 
 function Login() {
   const navigate = useNavigate();
@@ -13,7 +14,28 @@ function Login() {
     try { await loginUsuario(form); navigate("/dashboard"); } catch { setErro("E-mail ou senha inválidos."); }
   }
   return (
-    <main className="auth-page"><section className="auth-card"><div className="auth-icon"><LockKeyhole size={36} /></div><h1>Gerenciador de Senhas</h1><p>Acesse sua conta para gerenciar suas senhas.</p>{erro && <div className="alert alert-error">{erro}</div>}<form onSubmit={entrar}><label>E-mail<input name="email" type="email" value={form.email} onChange={alterarCampo} required /></label><label>Senha<input name="senha" type="password" value={form.senha} onChange={alterarCampo} required /></label><button className="btn btn-primary full-button" type="submit">Entrar</button></form><p className="auth-link">Não tem conta? <Link to="/cadastro">Cadastre-se</Link></p></section></main>
+    <main className="auth-page">
+      <section className="auth-card">
+        <div>< img src={logoNexoVault} alt="Logo NexoVault" className="auth-logo" /></div>
+        <div>
+          <h1>Gerenciador de Senhas</h1>
+        </div>
+        <p>Acesse sua conta para gerenciar suas senhas.</p>
+        {erro &&
+          <div className="alert alert-error">{erro}</div>}
+        <form onSubmit={entrar}>
+          <label>E-mail
+            <input name="email" type="email" value={form.email} onChange={alterarCampo} required />
+          </label>
+          <label>Senha<input name="senha" type="password" value={form.senha} onChange={alterarCampo} required />
+          </label>
+          <button className="btn btn-primary full-button" type="submit">Entrar</button>
+        </form>
+        <p className="auth-link">Não tem conta?
+          <Link to="/cadastro">Cadastre-se</Link>
+        </p>
+      </section>
+    </main>
   );
 }
 export default Login;
